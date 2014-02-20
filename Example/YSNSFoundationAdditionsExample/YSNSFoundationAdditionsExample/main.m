ffb71f7a02c7233c539527ef94ba7efacd8d3b0e
Yu Sugawara <yusuga@me.com>
2014-02-21 08:50:21 +0900

    Add Example

---
+++ Example/YSNSFoundationAdditionsExample/YSNSFoundationAdditionsExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YSNSFoundationAdditionsExample
+//
+//  Created by Yu Sugawara on 2014/02/21.
+//  Copyright (c) 2014年 Yu Sugawara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
