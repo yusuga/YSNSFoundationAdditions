a16a821531a3ca2776084c50c93fbf2a66ee23c9
Yu Sugawara <yusuga@me.com>
2014-04-04 18:06:04 +0900

    update NSStringTest

--- Example/YSNSFoundationAdditionsExample/YSNSFoundationAdditionsExampleTests/NSStringTest.m
+++ Example/YSNSFoundationAdditionsExample/YSNSFoundationAdditionsExampleTests/NSStringTest.m
@@ -79,4 +79,41 @@
                   @"str1: %@", [[@"" ys_escapedXMLString] ys_unescapedXMLString]);
 }
 
+#pragma mark - URL
+
+- (NSString*)URLString
+{
+    return @"文字列!*'();:@&=+$,/?%#[]._-";
+}
+
+- (NSString*)escapedURLString
+{
+    /* http://www.wap2.jp/cgi/escape/ */
+    return @"%E6%96%87%E5%AD%97%E5%88%97%21%2A%27%28%29%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23%5B%5D%2E%5F%2D";
+}
+
+- (void)testEscapeURL
+{
+    XCTAssertTrue([[[self URLString] ys_escapedURLString] isEqualToString:[self escapedURLString]],
+                  @"str1: %@, str2: %@", [[self URLString] ys_escapedURLString], [self escapedURLString]);
+    
+    XCTAssertFalse([[[self URLString] ys_escapedURLString] isEqualToString:[self URLString]],
+                   @"str1: %@, str2: %@", [[self URLString] ys_escapedURLString], [self URLString]);
+    
+    XCTAssertTrue([[@"" ys_escapedURLString] isEqualToString:@""],
+                  @"str1: %@", [@"" ys_escapedURLString]);
+}
+
+- (void)testUnescapeURLString
+{
+    XCTAssertTrue([[self URLString] isEqualToString:[[self escapedURLString] ys_unescapedURLString]],
+                  @"str1: %@, str2: %@", [self URLString], [[self escapedURLString] ys_unescapedURLString]);
+    
+    XCTAssertFalse([[self escapedURLString] isEqualToString:[[self escapedURLString] ys_unescapedURLString]],
+                   @"str1: %@, str2: %@", [self escapedURLString], [[self escapedURLString] ys_unescapedURLString]);
+    
+    XCTAssertTrue([[[@"" ys_escapedURLString] ys_unescapedURLString] isEqualToString:@""],
+                  @"str1: %@", [[@"" ys_escapedURLString] ys_unescapedURLString]);
+}
+
 @end
